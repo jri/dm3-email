f890fd4b2915b97529a410eef50d7d3c91245c1f
Jörg Richter <jri@deepamehta.de>
2009-11-09 17:10:02 +0100

    1) Adapted to current DeepaMehta 3 version. 2) An ant build file is included to support the plugin developer.

--- vendor/dm3-email/_attachments/script/dm3_email.js
+++ vendor/dm3-email/_attachments/script/dm3_email.js
@@ -1,5 +1,23 @@
 function dm3_email() {
+
     doctype_implementation("vendor/dm3-email/script/email.js")
+
+    this.init = function() {
+        types["Email"] = {
+            fields: [
+                {id: "From",    model: {type: "text"}, view: {editor: "single line", autocomplete_indexes: ["dm3-contacts"],                   autocomplete_style: "default"},   content: ""},
+                {id: "To",      model: {type: "text"}, view: {editor: "single line", autocomplete_indexes: ["dm3-contacts", "dm3-workspaces"], autocomplete_style: "item list"}, content: ""},
+                {id: "Cc",      model: {type: "text"}, view: {editor: "single line", autocomplete_indexes: ["dm3-contacts", "dm3-workspaces"], autocomplete_style: "item list"}, content: ""},
+                {id: "Bcc",     model: {type: "text"}, view: {editor: "single line", autocomplete_indexes: ["dm3-contacts", "dm3-workspaces"], autocomplete_style: "item list"}, content: ""},
+                {id: "Subject", model: {type: "text"}, view: {editor: "single line"},           content: ""},
+                {id: "Message", model: {type: "text"}, view: {editor: "multi line", lines: 18}, content: ""}
+            ],
+            view: {
+                icon_src: "vendor/dm3-email/images/envelope.png"
+            },
+            implementation: "Email"
+        }
+    }
 
     db.send_email = function(sender, recipients, subject, message, doc_id) {
         var uri = this.uri + "_mailer/" + doc_id
@@ -13,86 +31,5 @@ function dm3_email() {
         //    return null
         CouchDB.maybeThrowError(this.last_req)
         // return JSON.parse(this.last_req.responseText)
-    }
-}
-
-dm3_email.prototype = {
-
-    init: function() {
-        types["Email"] = {
-            fields: [
-                {
-                    id: "From",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "single line",
-                        autocomplete_indexes: ["dm3-contacts"],
-                        autocomplete_style: "default"
-                    },
-                    content: ""
-                },
-                {
-                    id: "To",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "single line",
-                        autocomplete_indexes: ["dm3-contacts", "dm3-workspaces"],
-                        autocomplete_style: "item list"
-                    },
-                    content: ""
-                },
-                {
-                    id: "Cc",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "single line",
-                        autocomplete_indexes: ["dm3-contacts", "dm3-workspaces"],
-                        autocomplete_style: "item list"
-                    },
-                    content: ""
-                },
-                {
-                    id: "Bcc",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "single line",
-                        autocomplete_indexes: ["dm3-contacts", "dm3-workspaces"],
-                        autocomplete_style: "item list"
-                    },
-                    content: ""
-                },
-                {
-                    id: "Subject",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "single line"
-                    },
-                    content: ""
-                },
-                {
-                    id: "Message",
-                    model: {
-                        type: "text",
-                    },
-                    view: {
-                        editor: "multi line",
-                        lines: 18
-                    },
-                    content: ""
-                }
-            ],
-            implementation: "Email",
-            icon_src: "vendor/dm3-email/images/envelope.png"
-        }
     }
 }
